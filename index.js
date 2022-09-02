920c6d6c3082f1c2d322b84691e6b55a0b87b517
chazi13 <m.chairul669@gmail.com>
2022-09-02 09:11:22 +0700

    feat: add route /hello

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/hello', (_, res) => {
+  res.send("yo! whatsapp")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
