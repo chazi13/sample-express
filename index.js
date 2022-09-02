aafd3de2c721af8d81a3e33d3b399d3480c408d6
chazi13 <m.chairul669@gmail.com>
2022-09-02 09:17:58 +0700

    add route /ci

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/hello', (_, res) => {
   res.send("yo! whatsapp")
 })
 
+app.get('/ci', (_, res) => {
+  res.send("test ci/cd")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
